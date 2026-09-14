c69966840fbdf7c9ded7263f4c3bf9783eecaef4
radicaltheend <igorzinluko@gmail.com>
2026-09-14 14:39:35 +0000

    Complete Lab 1

---
+++ lab01/hello.py
@@ -0,0 +1,4 @@
+name = input("Whats your name? ")
+course = input("Whats your course? ")
+area = input("Whats your favourite cybersec area? " )
+print("Hello, " + name + "\nYour course is: " + course + "\nYou are interested in: " + area) 
---
+++ lab01/student.py
@@ -0,0 +1,4 @@
+name = input("Whats your name? ")
+id = input("Whats your student ID? ")
+year = input("Which year are you in? ")
+print("Name: " + name + "\nStudent number: " + id + "\nYear: " + year)
